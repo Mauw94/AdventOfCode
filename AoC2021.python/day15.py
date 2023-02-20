9dbef17ee719beb0a42aa6f1d7dad89db01f1f57
Maurits <mauritsseelen@hotmail.com>
2023-02-20 13:07:55 +0100

    show differenecs in time between dijkstra and a*

--- AoC2021.python/day15.py
+++ AoC2021.python/day15.py
@@ -15,23 +15,27 @@ for i, line in enumerate(data):
         grid[(i, j)] = int(d)
 
 grid_expander = GridExpander(grid, width, height)
-grid, width, height = grid_expander.expand(10)
+grid, width, height = grid_expander.expand(5)
 
 start = (0, 0)
 end = (width - 1, height - 1)
-
 
 print("--- DIJKSTRA ---")
 start_time = time.time()
 dijkstra = Dijkstra(grid, width, height)
 distance = dijkstra.distance_start_to_end(start, end)
+d_time = (time.time() - start_time)
 print(distance)
-print("%s seconds" % (time.time() - start_time))
+print("%s seconds" % d_time)
 
 print("\n")
 print("--- A* ---")
 start_time = time.time()
 a_start = AStar(grid, width, height)
 distance = a_start.solve(start, end)
+a_time = (time.time() - start_time)
 print(distance)
-print("%s seconds" % (time.time() - start_time))
+print("\n")
+print("%s seconds" % a_time)
+
+print(round(((1 - (a_time / d_time)) * 100), 2), "% increase")
