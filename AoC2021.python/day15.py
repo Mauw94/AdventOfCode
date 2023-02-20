53254111028856004fb1747ee0c441c0503e42f6
Maurits <mauritsseelen@hotmail.com>
2023-02-20 10:23:33 +0100

    check execution time

--- AoC2021.python/day15.py
+++ AoC2021.python/day15.py
@@ -1,3 +1,4 @@
+import time
 from utils.expand_grid import GridExpander
 from utils.dijkstra import Dijkstra
 
@@ -6,6 +7,7 @@ data = open('input/day15_test.txt').read().splitlines()
 grid = {}
 width = len(data)
 height = len(data[0])
+start_time = time.time()
 
 for i, line in enumerate(data):
     for j, d in enumerate(line.strip()):
@@ -19,7 +21,8 @@ end = (width - 1, height - 1)
 
 dijkstra = Dijkstra(grid, width, height)
 distance = dijkstra.distance_start_to_end(start, end)
-distances = dijkstra.return_distances(start)
+# distances = dijkstra.return_distances(start)
 
 print(distance)
+print("--- %s seconds ---" % (time.time() - start_time))
 # print(distances)
