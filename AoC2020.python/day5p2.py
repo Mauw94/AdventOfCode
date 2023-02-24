3e0835f63979183dbd688669df0878ce94554723
Maurits <mauritsseelen@hotmail.com>
2023-02-24 13:52:21 +0100

    d5

---
+++ AoC2020.python/day5p2.py
@@ -0,0 +1,49 @@
+import math
+
+passes = open('input/day5.txt').read().splitlines()
+boarding_passes = []
+
+for p in passes:
+    row, col, seat_id = 0, 0, 0
+    range_r = [0, 127]
+    range_c = [0, 7]
+    rows = p[:7]
+    cols = p[7:]
+    for i, r in enumerate(rows):
+        if i == len(rows) - 1:
+            if r == "F":
+                row = min(range_r[0], range_r[1])
+            else:
+                row = max(range_r[0], range_r[1])
+        elif r == "F":
+            range_r = [range_r[0], math.floor(
+                range_r[1] / 2) + (range_r[0] / 2)]
+        else:
+            range_r = [math.ceil(range_r[1] / 2) +
+                       (range_r[0] / 2), range_r[1]]
+
+    for i, c in enumerate(cols):
+        if i == len(cols) - 1:
+            if c == "R":
+                col = max(range_c[0], range_c[1])
+            else:
+                col = min(range_c[0], range_c[1])
+        elif c == "L":
+            range_c = [range_c[0], math.floor(
+                range_c[1] / 2) + (range_c[0] / 2)]
+        else:
+            range_c = [math.ceil(range_c[1] / 2) +
+                       (range_c[0] / 2), range_c[1]]
+
+    seat_id = row * 8 + col
+    boarding_passes.append([row, col, seat_id])
+
+seats = set([b[2] for b in boarding_passes])
+all_seats = set(range(128 * 8))
+open_seats = all_seats - seats
+my_seat = [
+    seat
+    for seat in open_seats
+    if seat + 1 not in open_seats and seat - 1 not in open_seats]
+
+print(my_seat[0])
